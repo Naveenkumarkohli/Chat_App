6f369ccfbf9ea6fa6eb76947a953ce315051e037
Naveen <naveenkumarkohli06@gmail.com>
2025-09-19 16:03:14 +0530

    fix(ui): background image in production via imported SVG

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -5,21 +5,24 @@ import ProfilePage from './pages/ProfilePage'
 import {Toaster} from 'react-hot-toast'
 import { useContext } from 'react'
 import { AuthContext } from '../context/AuthContext'
+import bgImage from './assets/bgImage.svg'
 
 const App = () => {
 
   const {authUser} = useContext(AuthContext)
   return (
-    <div className="bg-[url('./src/assets/bgImage.svg')] bg-contain">
-     <Toaster />
-     <Routes>
-        <Route path='/' element={authUser?<HomePage />:<Navigate to="/login" />} />
-        <Route path='/login' element={!authUser? <LoginPage />: <Navigate to="/" />} />
-        <Route path='/profile' element={ authUser ?<ProfilePage />: <Navigate  to ="/login" />} />
-      </Routes>
-    </div>
+    <div
+      className="min-h-screen bg-black bg-no-repeat bg-cover bg-center"
+      style={{ backgroundImage: `url(${bgImage})` }}
+    >
+      <Toaster />
+      <Routes>
+         <Route path='/' element={authUser?<HomePage />:<Navigate to="/login" />} />
+         <Route path='/login' element={!authUser? <LoginPage />: <Navigate to="/" />} />
+         <Route path='/profile' element={ authUser ?<ProfilePage />: <Navigate  to ="/login" />} />
+       </Routes>
+     </div>
   )
 }
 
 export default App
-
